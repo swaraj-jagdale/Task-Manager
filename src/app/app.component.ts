4617fd8233de703c37c14ee75caa4f1bdbfed9c9
Jagdale, Swaraj <Swaraj_Jagdale@dell.com>
2024-08-16 14:51:33 +0530

    [14-08-2027] 2.46 Outputting Task Data in the Task Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,24 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selecteduserId?: string;
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selecteduserId);
+  }
 
   onSelectUser(id: string) {
-    console.log('Select user with id: ' + id);
+    this.selecteduserId = id;
   }
 }
